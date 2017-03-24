cff4dc92b2cafb22d947ad77df743aaa18a3a4fa
Mikhail Collins <kyleryker@gmail.com>
2017-03-24 10:36:18 -0600

    initital build

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const users = require('users'); 
+const users = require('./users.json');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+let app = express();
+app.use = (bodyParser.json());
+
+app.get('/api/users', function(req, res, next){
+res.status(200).json(users);
+});
+
+app.delete('/api/users/:id', function(req, res, next){
+  for (let i = 0; i < users.length; i++){
+    if (users[i].id = req.params.id){
+      users.splice(i - 1, 1);
+    }
+  }
+  res.status(200).json(users);
+});
+
+app.listen(3000, function(){
+  console.log('Listening on port 3000')
+})
